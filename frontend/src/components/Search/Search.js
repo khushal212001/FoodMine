83df5522e98c7d5ef2153dc1bad467a6dbb47e1a
Khushal <khushalgarg85886@gmail.com>
2024-04-06 23:58:15 +0530

    frontend functionality

---
+++ frontend/src/components/Search/Search.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import classes from './search.module.css'
+import { useNavigate, useParams } from 'react-router-dom'
+Search.defaultProps={
+  searchRoute:'/search/',
+  defaultRoute:'/',
+}
+
+
+const Search = ({searchRoute,defaultRoute,margin}) => {
+    const [term,setTerm] = useState('')
+    const navigate = useNavigate()
+    const {searchTerm} = useParams()
+
+    useEffect(()=>{
+      setTerm(searchTerm ?? '')
+    },[searchTerm])
+
+    const search = async ()=> {
+        term ? navigate(searchRoute + term) : navigate(defaultRoute)
+    }
+
+  return (
+    <div className={classes.container} style={{margin}} >
+        <input type='text' 
+            placeholder='Search Food' 
+            onChange={(e) => setTerm(e.target.value)} 
+            onKeyUp={(e)=> e.key === 'Enter' && search()}
+            value={term}
+        />
+        <button onClick={search}>Search</button>
+    </div>
+  )
+}
+
+export default Search
---
+++ frontend/src/pages/Home/HomePage.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useReducer } from 'react'
+import { getAll, getAllByTag, getAllTags, search } from '../../services/foodService'
+import Thumbnails from '../../components/Thumbnails/Thumbnails'
+import { useParams } from 'react-router-dom'
+import Search from '../../components/Search/Search'
+import Tags from '../../components/Tags/Tags'
+import NotFound from '../../components/NotFound/NotFound'
+const initialState = {foods: [],tags:[]}
+
+const reducer = (state,action)=>{
+  switch(action.type){
+    case 'FOODS_LOADED':
+      return {...state,foods:action.payload}
+    case 'TAGS_LOADED':
+      return {...state,tags:action.payload}
+    
+    default: 
+      return state
+  }
+}
+
+const HomePage = () => {
+  const [state,dispatch] = useReducer(reducer,initialState);
+  const {foods,tags} =state
+  const {searchTerm,tag} = useParams()
+
+  useEffect(()=>{
+
+    getAllTags().then(tags=> dispatch({type:'TAGS_LOADED',payload:tags}))
+    const loadedFoods = 
+      tag ? getAllByTag(tag)  
+      : searchTerm? search(searchTerm) : getAll()
+
+    loadedFoods.then(foods=> 
+      dispatch({type: 'FOODS_LOADED',payload:foods})
+    )
+    
+    // const fetchedData = async() =>{
+    // const foods = await getAll(); // Use await here
+    //   dispatch({ type: 'FOODS_LOADED', payload: foods });
+    // }
+    // fetchedData()
+  },[searchTerm,tag])
+  return (
+    <>
+      <Search/>
+      <Tags tags={tags} />
+      {foods.length === 0 && <NotFound linkText="Reset Search"/>}
+      <Thumbnails foods={foods}/>
+    </>
+  )
+}
+
+export default HomePage
